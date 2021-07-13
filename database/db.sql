f266d04b7109a5266e04bff55e8dd05836a93081
Mahaveer Narvekar <61501796+NarvekarMahaveer@users.noreply.github.com>
2021-07-13 19:34:45 +0530

    Updated database
    updated latest database

--- database/db.sql
+++ database/db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jul 13, 2021 at 07:37 AM
+-- Generation Time: Jul 13, 2021 at 02:03 PM
 -- Server version: 5.7.32
 -- PHP Version: 7.4.12
 
@@ -13,6 +13,125 @@ SET time_zone = "+00:00";
 --
 -- Database: `movie_inox`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `name`, `username`, `password`, `email`) VALUES
+(1, 'Admin', 'admin', 'admin', 'admin@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banner`
+--
+
+CREATE TABLE `banner` (
+  `id` int(11) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `heading` varchar(500) NOT NULL,
+  `sub_heading` varchar(500) NOT NULL,
+  `link` varchar(100) NOT NULL,
+  `link_txt` varchar(100) NOT NULL,
+  `order_number` int(11) NOT NULL,
+  `added_on` datetime NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `banner`
+--
+
+INSERT INTO `banner` (`id`, `image`, `heading`, `sub_heading`, `link`, `link_txt`, `order_number`, `added_on`, `status`) VALUES
+(1, '541939587_pngtree-steam-wave-international-film-festival-banner-poster-image_195212.jpg', 'Latest Movies', 'Watch your favourite movie', 'shop', 'View', 1, '2021-07-13 03:00:05', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `value` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `type`, `value`) VALUES
+(1, 'Language', 'English'),
+(2, 'Genre', 'Action'),
+(3, 'Language', 'Hindi'),
+(4, 'Genre', 'Comedy');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_us`
+--
+
+CREATE TABLE `contact_us` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `subject` varchar(200) NOT NULL,
+  `message` text NOT NULL,
+  `added_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `contact_us`
+--
+
+INSERT INTO `contact_us` (`id`, `name`, `email`, `mobile`, `subject`, `message`, `added_on`) VALUES
+(1, 'Veer', 'abc@gmail.com', '9999999999', 'Test Subject', 'test message', '2021-07-13 03:21:43');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movies`
+--
+
+CREATE TABLE `movies` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `featured_image` varchar(100) NOT NULL,
+  `movie_length` varchar(50) NOT NULL,
+  `released` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `movies`
+--
+
+INSERT INTO `movies` (`id`, `title`, `description`, `featured_image`, `movie_length`, `released`) VALUES
+(1, 'Kick', 'Salman Khan Movie', 'kick.jpg', '90 minutes', '2021-07-23'),
+(2, 'Jurassic Park', 'Dinosaurs movie', 'jurassic.jpg', '120 minutes', '2021-07-27'),
+(3, 'Fukrey', 'Comedy movie', 'fukrey.jpg', '60 minutes', '2021-08-17'),
+(4, 'Hacked', 'Web series', 'hacked.jpg', '80 minutes', '2021-07-18'),
+(5, 'DDLJ', 'Love story', 'ddlj.jpg', '67 minutes', '2021-07-17'),
+(6, 'Mohabbatein', 'Old movie', 'moha.jpg', '34 minutes', '2021-07-31'),
+(7, 'Go Goa Gone', 'Fun', 'goa.jpg', '39 minutes', '2021-09-22');
 
 -- --------------------------------------------------------
 
@@ -39,9 +158,67 @@ INSERT INTO `relationship` (`id`, `cat_id`, `movie_id`) VALUES
 (7, 4, 6),
 (8, 4, 7);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `status` int(11) NOT NULL,
+  `email_verify` int(11) NOT NULL,
+  `rand_str` varchar(20) NOT NULL,
+  `referral_code` varchar(20) NOT NULL,
+  `from_referral_code` varchar(20) NOT NULL,
+  `added_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `mobile`, `password`, `status`, `email_verify`, `rand_str`, `referral_code`, `from_referral_code`, `added_on`) VALUES
+(2, 'veer', 'veer@gmail.com', '9999999999', '$2y$10$cZ6sSDVYUINCnynmB3Tbuend7e7bRqgCPeD.RPmxdRucxKhbSo/52', 1, 1, 'lmkaetixyrzqoqy', 'lmkaetixyrzqoqds', '', '2021-07-13 05:11:37'),
+(5, 'AB Devilliers', 'abd@gmail.com', '9999999999', '$2y$10$wWBWMJTK37jkyLujN8YQoOVfQQt/3.B9ruoaNkjruQEwz4ZVfvNJa', 1, 1, 'zdoxxhajyeqtgpp', 'lhqtkjofzevfdai', 'lmkaetixyrzqoqds', '2021-07-13 05:11:37');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `banner`
+--
+ALTER TABLE `banner`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contact_us`
+--
+ALTER TABLE `contact_us`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `movies`
+--
+ALTER TABLE `movies`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `relationship`
@@ -52,14 +229,56 @@ ALTER TABLE `relationship`
   ADD KEY `movie_id` (`movie_id`);
 
 --
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `banner`
+--
+ALTER TABLE `banner`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `contact_us`
+--
+ALTER TABLE `contact_us`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `movies`
+--
+ALTER TABLE `movies`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `relationship`
 --
 ALTER TABLE `relationship`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
